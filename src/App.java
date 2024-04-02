55147dde52aa26047e9a96f6ba589ff1f31fba5c
Ryan Baber <rbaber@episcopalacademy.org>
2024-04-02 07:50:45 -0400

    initial commit

---
+++ src/App.java
@@ -0,0 +1,38 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        
+    }
+
+    // Write a function to countdown from a given number
+    // to 0 using recursion
+    public static void countdown(int start) {
+        System.out.print(start);
+    }
+
+    // Write a function to determine the sum of numbers
+    // from 1 to a given value using recursion
+    public static int sumNum(int n){
+        return 0;
+    }
+
+    // Write a function that uses recursion to generate
+    // the factorial of a given number 
+    public static int factorial(int n){
+        return 0;
+    }
+
+    // Write a function that uses recursion to return
+    // a number raised to a given power 
+    public static int power(int base, int pow){
+        return 0;
+    }
+
+    // Write a function that uses recursion to generate
+    // a fibonacci number by position (the parameter should
+    // be the position of the number, ie- at position 6, the
+    // fibonacci number is 8)
+    public static int fibonacci(int n){
+        return 0;
+    }
+}
